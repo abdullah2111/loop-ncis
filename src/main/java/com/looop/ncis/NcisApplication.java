4c08a888ccefd508ed10ed1e8a8d24e4bcd03f78
Abdullah Al Mamun <abdullah.official2111@gmail.com>
2026-03-06 10:46:55 +0600

    Initial commit

---
+++ src/main/java/com/looop/ncis/NcisApplication.java
@@ -0,0 +1,13 @@
+package com.looop.ncis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcisApplication.class, args);
+	}
+
+}
