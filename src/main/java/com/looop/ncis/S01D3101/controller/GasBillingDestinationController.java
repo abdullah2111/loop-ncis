5c00d6c69927e48a7b0615fcdbe7059ad2f97ec8
Shahinu Islam <shahinurislam963@gmail.com>
2026-03-11 09:20:51 +0600

    GasBillinDestination controller naming mismatching and url issue resolved

--- src/main/java/com/looop/ncis/S01D3101/controller/GasBillingDestinationController.java
+++ src/main/java/com/looop/ncis/S01D3101/controller/GasBillingDestinationController.java
@@ -11,9 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/gasbillingdestination")
+@RequestMapping("/api/v1/gas-billing-destinations")
 @RequiredArgsConstructor
-public class GasBillingDestination {
+public class GasBillingDestinationController {
     private final GasBillingDestinationService gasBillingDestinationService;
 
     @GetMapping("/info")
